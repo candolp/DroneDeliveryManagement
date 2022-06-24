b8decea550adbab4b8d573f7721091f025ec4ed0
candolp <candolpd@gmail.com>
2022-06-24 16:58:02 +0000

    Project Setup

---
+++ DroneTools/src/test/java/com/candolp/dronetools/DroneToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.candolp.dronetools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
